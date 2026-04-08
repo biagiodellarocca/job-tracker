be10527a0c4d72709d1b38784cf44c224273e879
Biagio <bdr@buerodespraesidenten.de>
2026-04-08 10:58:20 +0200

    update deploy infos

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-	baseURL: "http://localhost:9000/api/v1",
+	baseURL: "https://job-tracker-exfm.onrender.com/api/v1",
 });
 
 api.interceptors.request.use((config) => {
